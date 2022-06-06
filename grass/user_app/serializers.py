019619ade1c417370f8c6ec6a48e5fcd6cb8401a
al <egkashnikov@gmail.com>
2022-06-06 18:55:59 +0300

    csrf

--- grass/user_app/serializers.py
+++ grass/user_app/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
--- grass/user_app/views.py
+++ grass/user_app/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import UserSerializer
 from .models import CustomUser
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import action
 
 # Create your views here.
+@csrf_exempt
 class UserViewSet(ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = CustomUser.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-
