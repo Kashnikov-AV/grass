61c3be215b3a0bd39a2209228925bbb7916e87b8
al <egkashnikov@gmail.com>
2022-06-06 20:13:11 +0300

    g

--- grass/user_app/views.py
+++ grass/user_app/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import UserSerializer
 from .models import CustomUser
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import action
 
-# Create your views here.
-@csrf_exempt
+
 class UserViewSet(ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
