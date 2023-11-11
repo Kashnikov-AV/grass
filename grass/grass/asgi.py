538a26cd08270f74c2480d411a59e27aabab5cc1
Kashnikov-AV <egkashnikov@gmail.com>
2023-11-12 01:04:41 +0300

    change docker compose-daphne

--- grass/grass/asgi.py
+++ grass/grass/asgi.py
@@ -13,4 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grass.settings')
 
-django_asgi_app = get_asgi_application()
+application = get_asgi_application()
