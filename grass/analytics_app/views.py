b2e35e96ce7a70519cf5e7274dec75b02c990035
Aureilius <aureilius@mail.ru>
2023-12-08 12:32:32 +0300

    Пытаюсь закоммитить файл

--- grass/analytics_app/views.py
+++ grass/analytics_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def graphs(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'analytics_app/graphs.html')
