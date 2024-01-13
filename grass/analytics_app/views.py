8e6be187ec8a52a723af862c98f36e6c05273be0
Kashnikov-AV <egkashnikov@gmail.com>
2024-01-13 22:57:04 +0300

    create pie chart workexp

--- grass/analytics_app/views.py
+++ grass/analytics_app/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import plotly.express as px
-import vacancy_app.models
-import profile_app.models
+from vacancy_app.models import Vacancy, WORKEXP_CHOICES
+from profile_app.models import Company, Profile, ExpJob, Education
+from django.db.models import Count
+import pandas as pd
+
+def get_req_exp_chart():
+    # Pie Chart
+    workexp_count = Vacancy.objects.values('work_experience').annotate(cnt=Count('work_experience'))
+    dwe = dict(WORKEXP_CHOICES)
+    for x in workexp_count:
+        x['work_experience'] = dwe[x['work_experience']]
+
+    df = pd.DataFrame(workexp_count)
+    print(df)
+    fig_pie = px.pie(df, values='cnt', names='work_experience', height=350)
+    fig_pie.update_layout(legend=dict(
+        yanchor="top",
+        y=-0.05,
+        xanchor="left",
+        x=0.01
+    ))
+    exp_chart = fig_pie.to_html(full_html=False, include_plotlyjs=False)
+    return exp_chart
+
 
 # Create your views here.
 @login_required
@@ -23,18 +45,17 @@ def draw_charts(request):
     fig_line = px.line(df, x="lifeExp", y="gdpPercap", color="country", text="year", height=300)
     fig_line.update_traces(textposition="bottom right")
 
-    # Pie Chart
-    df = px.data.tips()
-    fig_pie = px.pie(df, values='tip', names='day', height=300)
+
 
     bar_chart = fig_bar.to_html(full_html=False, include_plotlyjs=False)
     scatter_chart = fig_scatter.to_html(full_html=True, include_plotlyjs=False)
     line_chart = fig_line.to_html(full_html=False, include_plotlyjs=False)
-    pie_chart = fig_pie.to_html(full_html=False, include_plotlyjs=False)
+
+    req_exp_chart = get_req_exp_chart()
 
     return render(request, "analytics_app/analytics.html", {"bar_chart": bar_chart ,
                                                             "scatter_chart": scatter_chart,
                                                             "line_chart": line_chart,
-                                                            "pie_chart": pie_chart,
+                                                            "pie_chart": req_exp_chart,
                                                             }
                   )
