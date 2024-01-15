c6d3db55fb8ca0de44c345de3764382b4ea043bc
Griffith <Aureilius@mail.ru>
2024-01-15 14:07:24 +0300

    Создал график hist sellary

--- grass/analytics_app/views.py
+++ grass/analytics_app/views.py
@@ -6,6 +6,14 @@ from profile_app.models import Company, Profile, ExpJob, Education
 from django.db.models import Count
 import pandas as pd
 
+def get_avg_sellary_hist():
+    min_sellary = Vacancy.objects.values('salary_min')
+    df = pd.DataFrame(min_sellary)
+    print(df)
+    fig_hist = px.histogram(df)
+    sellary_hist = fig_hist.to_html(full_html=False, include_plotlyjs=False)
+    return sellary_hist
+
 def get_req_exp_chart():
     # Pie Chart
     workexp_count = Vacancy.objects.values('work_experience').annotate(cnt=Count('work_experience'))
@@ -14,7 +22,6 @@ def get_req_exp_chart():
         x['work_experience'] = dwe[x['work_experience']]
 
     df = pd.DataFrame(workexp_count)
-    print(df)
     fig_pie = px.pie(df, values='cnt', names='work_experience', height=350)
     fig_pie.update_layout(legend=dict(
         yanchor="top",
@@ -30,11 +37,6 @@ def get_req_exp_chart():
 @login_required
 def draw_charts(request):
 
-    # Bar Chart
-    wide_df = px.data.medals_wide()
-
-    fig_bar = px.bar(wide_df, x="nation", y=["gold", "silver", "bronze"], title="Wide-Form Input", height=300)
-
     # Scatter Chart
     df = px.data.iris()
     fig_scatter = px.scatter(df, x="sepal_width", y="sepal_length", color="species", height=300, hover_data=['petal_width'])
@@ -47,13 +49,12 @@ def draw_charts(request):
 
 
 
-    bar_chart = fig_bar.to_html(full_html=False, include_plotlyjs=False)
     scatter_chart = fig_scatter.to_html(full_html=True, include_plotlyjs=False)
     line_chart = fig_line.to_html(full_html=False, include_plotlyjs=False)
-
+    min_sellary_hist = get_avg_sellary_hist()
     req_exp_chart = get_req_exp_chart()
 
-    return render(request, "analytics_app/analytics.html", {"bar_chart": bar_chart ,
+    return render(request, "analytics_app/analytics.html", {"bar_chart": min_sellary_hist,
                                                             "scatter_chart": scatter_chart,
                                                             "line_chart": line_chart,
                                                             "pie_chart": req_exp_chart,
