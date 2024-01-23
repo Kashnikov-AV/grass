8b220ddf11eabb0b1a772d445554e7b8f5380c41
Kashnikov-AV <egkashnikov@gmail.com>
2024-01-24 02:49:14 +0300

    поправил меню на главной

--- grass/chat_app/consumers.py
+++ grass/chat_app/consumers.py
@@ -10,14 +10,19 @@ User = get_user_model()
 class PersonalChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         my_id = self.scope['user'].id
+
         other_user_id = self.scope['url_route']['kwargs']['id']
+        other_user = User.objects.get(pk=other_user_id)
+
         if int(my_id) > int(other_user_id):
             self.room_name = f'{my_id}-{other_user_id}'
         else:
             self.room_name = f'{other_user_id}-{my_id}'
 
         self.room_group_name = 'chat_%s' % self.room_name
-        await Room.add(self.room_group_name, self.scope.get('user'))
+
+        Room.add(self.room_group_name, self.scope.get('user'))
+        Room.add(self.room_group_name, other_user)
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -62,10 +67,13 @@ class PersonalChatConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def save_message(self, username, thread_name, message, receiver):
         room_obj = Room.objects.get(room_name=thread_name)
+
         chat_obj = ChatModel.objects.create(
             sender=username, message=message, thread_name=thread_name, room=room_obj)
+
         other_user_id = self.scope['url_route']['kwargs']['id']
         get_user = User.objects.get(id=other_user_id)
+
         if receiver == get_user.username:
             ChatNotification.objects.create(chat=chat_obj, user=get_user)
 
