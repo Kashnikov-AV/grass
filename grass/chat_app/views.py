8d668b0267ccb7757b1a1bd3ec4eaed6fd3f0f32
Kashnikov-AV <egkashnikov@gmail.com>
2024-01-24 00:27:08 +0300

    чат страницы

--- grass/chat_app/views.py
+++ grass/chat_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from chat_app.models import ChatModel
+from chat_app.models import ChatModel, Room
 
 # Create your views here.
 User = get_user_model()
@@ -26,6 +26,8 @@ def chat(request):
 def chatPage(request, email):
     user_obj = User.objects.get(email=email)
 
+
+
     rooms = request.user.rooms.all()
     users_id = set()
     for r in rooms:
@@ -39,7 +41,13 @@ def chatPage(request, email):
     else:
         thread_name = f'chat_{user_obj.id}-{request.user.id}'
 
+    current_room = Room.objects.get(room_name=thread_name)
     users_list = User.objects.filter(pk__in=list(users_id)).exclude(pk=request.user.pk)
 
     message_objs = ChatModel.objects.filter(thread_name=thread_name)
-    return render(request, 'chat_app/chat-page.html', context={'userobj': user_obj, 'users': users_list, 'messages': message_objs})
+    return render(request, 'chat_app/chat-page.html', context={'userobj': user_obj,
+                                                               'users': users_list,
+                                                               'messages': message_objs,
+                                                               'room_obj': current_room
+                                                               }
+                  )
